a5b78454207924fb6c9d3edf488f84890b00fca6
Aayush Kumar Gupta <aayush@itobuz.com>
2024-12-27 15:15:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function aqua(){
+  document.body.style.backgroundColor = "aqua"
+}
+
+function aquamarine(){
+  document.body.style.backgroundColor = "aquamarine"
+}
+
+function bisque(){
+  document.body.style.backgroundColor = "bisque"
+}
+
+function beige(){
+  document.body.style.backgroundColor = "beige"
+}
