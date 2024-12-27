69d051e1776a734145e08993c4553827e16c1248
Aayush Kumar Gupta <aayush@itobuz.com>
2024-12-27 16:28:02 +0530

    modified style.scss

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-
-
 function colorChange() {
-  let color = document.getElementById("changer");
+  const color = document.getElementById("changer");
   const minm = 100000;
   const maxm = 999999;
-  let hexCode = Math.floor(Math.random() * (maxm - minm + 1)) + minm;
-  color.innerHTML = "color code : #" + hexCode;
+  const hexCode = Math.floor(Math.random() * (maxm - minm + 1)) + minm;
   const bgColor = "#" + hexCode;
+
+  color.innerHTML = "Color code : #" + hexCode;
   document.body.style.backgroundColor = bgColor;
 }
 
