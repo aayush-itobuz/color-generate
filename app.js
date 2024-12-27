2416b40abd0031d42aa621b60e11a692191dc918
Aayush Kumar Gupta <aayush@itobuz.com>
2024-12-27 16:16:49 +0530

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-function aqua(){
-  document.body.style.backgroundColor = "aqua"
+
+
+function colorChange() {
+  let color = document.getElementById("changer");
+  const minm = 100000;
+  const maxm = 999999;
+  let hexCode = Math.floor(Math.random() * (maxm - minm + 1)) + minm;
+  color.innerHTML = "color code : #" + hexCode;
+  const bgColor = "#" + hexCode;
+  document.body.style.backgroundColor = bgColor;
 }
 
-function aquamarine(){
-  document.body.style.backgroundColor = "aquamarine"
-}
-
-function bisque(){
-  document.body.style.backgroundColor = "bisque"
-}
-
-function beige(){
-  document.body.style.backgroundColor = "beige"
-}
